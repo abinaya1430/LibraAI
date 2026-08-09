0c8dd1061b6581054738a9efcd9808df3f6a9449
abinaya1430 <abinayaanand1430@gmail.com>
2026-08-09 13:33:27 +0530

    Update LibraAI.py

--- LibraAI.py
+++ LibraAI.py
@@ -111,56 +111,35 @@ def create_book_list(book_list):
         <div style="
 
             padding:20px;
-
             margin:14px 0;
-
             border-radius:18px;
-
             background:#90D5FF;
-
             border:1px solid #30394f;
-
             text-align:left;
-
             color:#000000;
-
-        ">
-
-            <div style="
-
-                font-size:22px;
-
-                font-weight:bold;
-
-                margin-bottom:12px;
-
             ">
 
-                📚 {book[0]}
-
+            <div style="
+            font-size:22px;
+            font-weight:bold;
+            margin-bottom:12px;
+            ">
+            📚 {book[0]}
             </div>
 
             <div style="margin:8px 0;">
-
-                👤 <b>Author:</b> {book[1]}
-
+            👤 <b>Author:</b> {book[1]}
             </div>
 
             <div style="margin:8px 0;">
-
-                🏷️ <b>Category:</b> {book[2]}
-
+            🏷️ <b>Category:</b> {book[2]}
             </div>
 
             <div style="margin:8px 0;">
-
-                📌 <b>Status:</b> {status_badge(book[3])}
-
+            📌 <b>Status:</b> {status_badge(book[3])}
             </div>
-
-        </div>
-
-        """
+            </div>
+            """
 
     html += "</div>"
 
